39583be37fc3147a189b31af3994954aa3b04168
zztkm <takumi.basket1682@gmail.com>
2022-10-15 02:41:16 +0900

    python to v での文字列受け渡しはできた

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ def main():
     lib.sqrt_of_sum_of_squares.restype = c_double
     assert lib.sqrt_of_sum_of_squares(c_double(1.1), c_double(2.2)) == math.sqrt(1.1*1.1 + 2.2*2.2), "Cannot validate V sqrt_of_sum_of_squares()."
 
-    hello = lib.hello("zztkm")
-    print(hello)
-    print("end")
+    # 文字列のやり取りテスト
+    hello: int = lib.hello(b"zztkm")
+    # print(hello.decode("utf-8"))
+    print(hello) # int が返ってきてるっぽい
 
 
 if __name__ == "__main__":
--- test.v
+++ test.v
@@ -13,8 +13,8 @@ fn sqrt_of_sum_of_squares(x f64, y f64) f64 {
 }
 
 [export: 'hello']
-fn hello(name &char) &char {
-	v_name := unsafe { cstring_to_vstring(name) }
+fn hello(name &u8) u8 {
+	v_name := unsafe { name.vstring() }
 	println(v_name)
-	return unsafe { name }
+	return 'hello, $v_name!'.u8()
 }
